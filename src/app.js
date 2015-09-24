0cf5f55cfaa0fb763f618a02c462b3a09f020226
schap18 <schap18@users.noreply.github.com>
2015-09-24 11:39:54 +0200

    WindowTest

--- src/app.js
+++ src/app.js
@@ -1,27 +1,23 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
+
 
 var UI = require('ui');
-var ajax = required('ajax');
-var Vector2= required('vector2');
+var ajax = require('ajax');
+var Vector2 = require('vector2');
 
-// Show splash screen while waiting for Mediupdate
+//show splash screen while waiting for data
 var splashWindow = new UI.Window();
 
-//Test elemt to inform user
+//Text element to inform user
 var text = new UI.Text({
-	position: new Vector2(0, 0),
-	size: new Vector2(144, 168),
-	text: 'Updateing your Medication...',
-	font: 'GOTHIC_28_BOLD'
+	position: new Vector2(0, 0), 
+	size: new Vector2(130, 140),
+	text:'Updateing your Medication...', 
+	font: 'GOTHIC_28_BOLD', 
 	color:'black',
-	textOverflow:'warp'
-	textAlign:'center',
-	backgroundColor:'white'
-});
+	textOverflow:'wrap', 
+	textAlign: 'center', 
+	backgroundColor: 'white'});
+
 
 //Add to splashWindow and show
 splashWindow.add(text);
