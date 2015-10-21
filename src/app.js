f70bf4656e78173d0db9a563d8258eec4bca388d
schap18 <schap18@users.noreply.github.com>
2015-10-21 11:14:08 +0200

    Mediliste mit Pebble.js
    Details noch nicht funktionstüchtig.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,57 @@
-
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
 
 var UI = require('ui');
-var ajax = require('ajax');
-var Vector2 = require('vector2');
 
-//show splash screen while waiting for data
-var splashWindow = new UI.Window();
+//Create a Card with title and subtitle
+//var card = new UI.Card({
+	//title:'Medication',
+	//subtitle:'Fetching...'
+	//button type ="button"
+//})
 
-//Text element to inform user
-var text = new UI.Text({
-	position: new Vector2(0, 0), 
-	size: new Vector2(130, 140),
-	text:'Updateing your Medication...', 
-	font: 'GOTHIC_28_BOLD', 
-	color:'black',
-	textOverflow:'wrap', 
-	textAlign: 'center', 
-	backgroundColor: 'white'});
+//Display the Card
+//card.show();
 
+//Make a list of menue items
+var Medication = [
+	{
+		title: "Dafalgan",
+		subtitle: "Tabl"
+	},
+	{
+		title: "Aspirin",
+		subtitle: "Brause Tabl"
+	},
+	{
+		title: "Pantozol",
+		subtitle: " Tabl"
+	}
+];
 
-//Add to splashWindow and show
-splashWindow.add(text);
-splashWindow.show();
+//Create the Menu, supplying the list of fruits
+var mediMenu = new UI.Menu ({
+	sections: [{
+		title: 'Medication List',
+		items: Medication
+	}]
+});
+
+//Show the Menu
+mediMenu.show();
+
+//Add a click listeneer for selection button click
+mediMenu.on('select', function(event){
+	
+	//Show a card with clicked item details
+	var detailCard = new UI.Card({
+		title: Medication = [event.itemIndex].title,
+		body: Medication = [event.itemIndex].subtitle
+	});
+	
+	//Show the new Card
+	detailCard.show();
+});
